00b9121fdfc2401335b177d869959f6bd104d341
Nicholas Wease <nicholaswease@Nicholass-MacBook-Air.local>
2021-03-12 12:15:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import * as ml5 from 'ml5';
+import {useRef, useEffect, useState} from 'react';
+// import sunrise from './assets/sunrise.jpeg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const imageRef= useRef();
+    const [file, setFile] = useState(null);
+    const [results, setResults] = useState([]);
+
+    const classifyImg = () => {
+        const classifier = ml5.imageClassifier('MobileNet', () => console.log('Model Loaded!'));
+
+        classifier.predict(imageRef.current, 5, (err, results) => {
+            console.log(results);
+            
+            setResults(results);
+        })
+    }
+
+    useEffect(() => {
+        classifyImg();
+    }, [file])
+
+    const fileSelectedHandler = (e) => {
+        let selectedFile = e.target.files[0];
+        let reader = new FileReader();
+
+        imageRef.current.title = selectedFile.name;
+
+        reader.onload = function(e) {
+            imageRef.current.src = e.target.result;
+        }
+
+        setFile(e.target.files[0]);
+
+        reader.readAsDataURL(selectedFile);
+    }
+
+    console.log(file);
+
+    return (
+        <div className='app'>
+            <h1>
+                HELLO
+            </h1>
+
+            <img
+                ref={imageRef}
+                alt=''
+            />
+
+            <input
+                type='text'
+                type='file'
+                onChange={fileSelectedHandler}
+            />
+            
+            {
+                results?.map(({label, confidence}) => (
+                    <div key={confidence}>
+                        <p>
+                            {label}: {confidence}
+                        </p>
+                    </div>
+                ))
+            }
+        </div>
+    );
 }
 
 export default App;
